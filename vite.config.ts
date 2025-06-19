4e8e7d2d29b42b408c157988d27f2417cb52ecdf
Jack Fan <jack.fan.dev@gmail.com>
2025-06-19 14:36:29 -0400

    fixes cuz vibes don't always vibe

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
+import { loadEnv } from "vite";
 
 // https://vite.dev/config/
+const env = loadEnv("development", process.cwd(), "");
 export default defineConfig({
   plugins: [react()],
   base: "./",
@@ -10,5 +12,8 @@ export default defineConfig({
     postcss: {
       plugins: [tailwind()],
     },
+  },
+  define: {
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
   },
 });
